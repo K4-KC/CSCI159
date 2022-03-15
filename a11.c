031fc1f5d9f49100e1cafe169ae28d3c20a82bad
K4_keyChan_ <72117307+K4-KC@users.noreply.github.com>
2022-03-15 09:36:05 -0400

    Create a11.c

---
+++ a11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int get_number(void);		
+int ipow(int base, int exp);
+void convert_to_binary(int n);
+
+int main(void) {
+  int number;
+  number = get_number();
+  convert_to_binary(number);
+  return 0;
+}
+
+int get_number(void) {
+  
+}
