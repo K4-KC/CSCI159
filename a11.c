aad4014db99a14c08b65788b4de8c439e85a3c1d
K4_keyChan_ <72117307+K4-KC@users.noreply.github.com>
2022-03-15 09:47:31 -0400

    Update a11.c

--- a11.c
+++ a11.c
@@ -1,16 +1,54 @@
+/******************************************************************************
+
+This program converts base 10 numbers to binary
+
+Author: Kanva Kanva
+
+*******************************************************************************/
+
+#include <stdio.h>
 #include <stdio.h>
 
-int get_number(void);		
+int get_number();		
 int ipow(int base, int exp);
 void convert_to_binary(int n);
 
 int main(void) {
   int number;
+  
   number = get_number();
   convert_to_binary(number);
+  
   return 0;
 }
 
-int get_number(void) {
+int get_number() {
+  int number;
+  do {
+       printf("\nEnter the decimal number (from 0 to 1023):  ");
+       scanf("%d", &number);
+    } while(number > 1023 || number < 0);
   
+  return number;
+}
+
+int ipow(int base, int exp) {
+  int p = 1;
+  for(int i = 0; i < exp; i++) {
+    p *= base;
+  }
+  
+  return p;
+}
+
+void convert_to_binary(int n) {
+  for(int i = 9; i >= 0; i--) {
+    int bin_dig = ipow(2, i);
+    if(bin_dig <= n) {
+      printf("1");
+      n -= bin_dig;
+    } else {
+      printf("0");
+    }
+  }
 }
